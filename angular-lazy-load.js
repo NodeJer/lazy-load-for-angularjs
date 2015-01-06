f9ecf13362e0c51f235229390a2cb36df5eea9ad
Cole <taoye1245089789@gmail.com>
2015-01-06 16:08:11 +0800

    first

--- angular-lazy-load.js
+++ angular-lazy-load.js
@@ -1,23 +1,85 @@
 angular.module('imgLazyLoad', []).
 
-factory('findRollingParent', function(){
-	return function(obj){
-		var parent = obj.parentNode;
-		var overflow = tools.getStyle(parent, 'overflow');
+factory('tools', function(){
+	var tools = {
+		findRollingParent: function(obj){
+			var parent = obj.parentNode;
+			var overflow = tools.getStyle(parent, 'overflow');
 
-		while(parent && parent.tagName!=='BODY' && overflow !== 'auto' && overflow !== 'scroll' ) {
-			parent = parent.parentNode;
-			overflow = tools.getStyle(parent, 'overflow');
+			while(parent && parent.tagName!=='BODY' && overflow !== 'auto' && overflow !== 'scroll' ) {
+				parent = parent.parentNode;
+				overflow = tools.getStyle(parent, 'overflow');
+			}
+
+			return parent;
+		},
+		offset: function(element, parentWin){
+			if(parentWin === window || parentWin === document){
+				parentWin = document.body;
+			}
+			parentWin.style.position = 'relative';
+
+			var offsetTop = element.offsetTop;
+			var offsetLeft = element.offsetLeft;
+
+			var offset = {
+				top: offsetTop,
+				left: offsetLeft
+			};
+			while(element.offsetParent && element.offsetParent !== parentWin){
+				offset.top+=element.offsetParent.offsetTop;
+				offset.left+=element.offsetParent.offsetLeft;
+			}
+
+			return offset;
+			
+		},
+		isVisible: function(element, parentWin){
+
+			var windowHeight;
+			var scrollTop;
+
+			if(parentWin.tagName === 'BODY'|| parentWin === window||parentWin === document){
+				windowHeight = document.documentElement.clientHeight||document.body.clientHeight;
+				scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+			}
+			else{
+				windowHeight = parentWin.clientHeight;
+				scrollTop = parentWin.scrollTop;
+			}
+			
+
+			var offset = this.offset(element, parentWin);
+			
+			var offsetHeight = element.offsetHeight;
+
+			var ws = windowHeight+scrollTop;
+
+			if(offset.top < ws && offsetHeight+offset.top > scrollTop){
+				return true;
+			}
+			return false;
+		},
+		getStyle: function(obj, attr){
+			var res;
+
+			if(obj.currentStyle){
+				res = obj.currentStyle[attr];
+			}
+			else{
+				res = getComputedStyle(obj, false)[attr];
+			}
+			return res;
 		}
+	};
 
-		return parent;
-	}
+	return tools;
 }).
 
-directive('lazyLoad', function(findRollingParent){
+directive('lazyLoad', function(tools){
 	return {
 		link: function($scope, elements, attrs, configCtrl) {
-			var parentScollWindow = findRollingParent(elements[0]);
+			var parentScollWindow = tools.findRollingParent(elements[0]);
 			    parentScollWindow = parentScollWindow.tagName === 'BODY'? window : parentScollWindow;
 
 			var src = attrs.lazyLoad;
